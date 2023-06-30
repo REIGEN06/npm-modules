915ce2be6fb2e538b189788293b61f504e0c9f4e
Daniil Yakubov <daniil-yak@yandex.ru>
2023-06-30 22:04:56 +0300

    feat: изменение названия функции

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useDocumentVisible } from "./hooks/useDocumentVisible.js";
-import { useState, useEffect } from "react";
+import { useDocumentVisibility } from "./hooks/useDocumentVisibility.js";
+import {useEffect } from "react";
 
 function App() {
-  const { count, visible, onVisibilityChange } = useDocumentVisible(0);
+  const { count, visible, onVisibilityChange } = useDocumentVisibility(0);
 
   useEffect(() => {
+    onVisibilityChange((isVisible) => {
+      console.log('first handler', isVisible)
+    });
+
+    const unsubscribeSecondHandler = onVisibilityChange((isVisible) => {
+      console.log('second handler', isVisible)
+    });
+
+    setTimeout(() => unsubscribeSecondHandler, 5000); // отписываемся от 'second handler' через 5 секунд
+
     window.addEventListener("visibilitychange", onVisibilityChange);
+    console.log('setup');
     return () => {
       window.removeEventListener("visibilitychange", onVisibilityChange);
+      console.log('setout');
     };
   }, [visible]);
 
---
+++ src/hooks/useDocumentVisibility.js
@@ -0,0 +1,19 @@
+import { useCallback, useState } from "react";
+
+export const useDocumentVisibility = (initialValue = 0) => {
+  let [count, setCount] = useState(initialValue);
+  let [visible, setVisible] = useState(true);
+
+  const increment = () => setCount((currentCount) => currentCount + 1);
+  
+  //Можно работать через document.visibilityState, но тогда придется
+  // обрабатывать строковые значения. Сейчас все работает через boolean
+  const onVisibilityChange = () => {
+    setVisible(!document.hidden); //потому что не существует метода document.visible
+    if (!visible){
+      increment();
+    }
+  };
+
+  return { count, visible, onVisibilityChange };
+};
