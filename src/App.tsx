0e5c18222585fe532aefa49ecf142f435f6117d4
Даниил Якубов <dyakubov@web-bee.ru>
2023-07-01 21:52:56 +0300

    feat: перенос проекта на tsx, подключение пакета

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { DocumentVisibility } from "./UseDocumentVisibility/UseDocumentVisibility";
+
+function App() {
+  return (
+    <div className="App">
+      <DocumentVisibility />
+    </div>
+  );
+}
+
+export default App;
