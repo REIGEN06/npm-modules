703e38b1ae1345fa82266985a9f2cfa6efcefc64
Даниил Якубов <dyakubov@web-bee.ru>
2023-07-01 15:49:06 +0300

    feat: добавил ts

--- src/hooks/useDocumentVisibility.js
+++ src/hooks/useDocumentVisibility.js
@@ -1,33 +1,39 @@
-import { useEffect, useState } from "react";
-
-export const useDocumentVisibility = (initialValue = 0) => {
-  const [count, setCount] = useState(initialValue);
-  const [visible, setVisible] = useState(true);
-  const [callbacks, setCallbacks] = useState([]);
-
-  const increment = () => setCount((currentCount) => currentCount + 1);
-
-  const onVisibilityChange = (callback) => {
-    setCallbacks((callbacks) => [...callbacks, callback]);
-  };
-
-  const Handle = () => {
-    setVisible(document.visibilityState === "visible");
-    if (document.hidden) {
-      increment();
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
     }
-    callbacks.forEach((callback) => {
-      callback(document.visibilityState === "visible");
-    });
-  };
-
-  useEffect(() => {
-    document.addEventListener("visibilitychange", Handle);
-    return () => {
-      document.removeEventListener("visibilitychange", Handle);
-    };
-  }, [callbacks]);
-  console.log(callbacks);
-
-  return { count, visible, onVisibilityChange };
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useDocumentVisibility = void 0;
+var react_1 = require("react");
+var useDocumentVisibility = function () {
+    var _a = (0, react_1.useState)(0), count = _a[0], setCount = _a[1];
+    var _b = (0, react_1.useState)(true), visible = _b[0], setVisible = _b[1];
+    var _c = (0, react_1.useState)([]), callbacks = _c[0], setCallbacks = _c[1];
+    var increment = function () { return setCount(function (currentCount) { return currentCount + 1; }); };
+    var onVisibilityChange = function (callback) {
+        setCallbacks(function (callbacks) { return __spreadArray(__spreadArray([], callbacks, true), [callback], false); });
+    };
+    var Handle = function () {
+        setVisible(document.visibilityState === "visible");
+        if (document.hidden) {
+            increment();
+        }
+        callbacks.forEach(function (callback) {
+            callback(document.visibilityState === "visible");
+        });
+    };
+    (0, react_1.useEffect)(function () {
+        document.addEventListener("visibilitychange", Handle);
+        return function () {
+            document.removeEventListener("visibilitychange", Handle);
+        };
+    }, [callbacks]);
+    return { count: count, visible: visible, onVisibilityChange: onVisibilityChange };
+};
+exports.useDocumentVisibility = useDocumentVisibility;
