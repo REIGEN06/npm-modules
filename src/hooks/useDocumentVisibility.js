b04cacc297c21410850896962a25087589984b87
Даниил Якубов <dyakubov@web-bee.ru>
2023-07-01 11:08:52 +0300

    feat: своя проверка работоспособности

--- src/hooks/useDocumentVisibility.js
+++ src/hooks/useDocumentVisibility.js
@@ -1,16 +1,17 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 export const useDocumentVisibility = (initialValue = 0) => {
   let [count, setCount] = useState(initialValue);
   let [visible, setVisible] = useState(true);
 
   const increment = () => setCount((currentCount) => currentCount + 1);
-  
+
   //Можно работать через document.visibilityState, но тогда придется
   // обрабатывать строковые значения. Сейчас все работает через boolean
   const onVisibilityChange = () => {
     setVisible(!document.hidden); //потому что не существует метода document.visible
-    if (!visible){
+    console.log("isVisible:", !visible);
+    if (!visible) {
       increment();
     }
   };
